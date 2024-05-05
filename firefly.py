3e0940d04e8c87788c17a53af4232db56bbc2393
Dolf7 <rudolf.saragih97@gmail.com>
2024-05-05 21:50:05 +0700

    050524-2150: update firefly-class algo, update running: extract data and run fa algo

--- firefly.py
+++ firefly.py
@@ -1,10 +1,12 @@
 from random import random
 from math import exp
+from math import e 
 from math import sqrt
+
 
 class firefly_algo:
 
-    def __init__(self,data, population, max_iter, low_bound=0, up_bound=1, gamma=1, alpha=0.1, beta=0.2, damping_ratio=0.98, m=2):
+    def __init__(self,data, population:int, max_iter:int, low_bound=0, up_bound=1, gamma=0.01, alpha=0.1, beta=0.2, delta=0.97):
         self.data = data #store pos
         self.population = population
         self.iter = max_iter;
@@ -13,51 +15,53 @@ class firefly_algo:
         self.gamma = gamma
         self.alpha = alpha
         self.beta = beta
-        self.delta = 0.05*(self.up_bound - self.low_bound)
-        self.damping_rat = damping_ratio;
-        self.m = m
-        self.fitness_value = self.fitness_function() 
+        self.delta = delta
+        self.scale = abs(up_bound - low_bound)
+        self.fitness_value = self.fitness_set_all() 
         self.movement = [] # store the history of pos
         self.movement_fitness = [] # store the history of cost
     
-    def fitness_function(self): # count the cost
+    def fitness_function(self, x):
+        return x**2
+    
+    def fitness_set_all(self): # count the cost
         mod_data = []
         for i in self.data:
-            mod_data.append(i**2)
+            mod_data.append(self.fitness_function(i))
         return mod_data;
 
-    def update_data(self):
-        self.movement.append(self.data)
-        self.movement_fitness.append(self.fitness_value)
-
     def run(self):
-        self.update_data()
-        for i in range(self.population):
-            for j in range(self.population):
-                if(self.fitness_value[i] < self.fitness_value[j]):
-                    self.data[j] = self.move_to(i, j)
-                else:
-                    self.data[j] = self.move_random(i)
-            self.fitness_value = self.fitness_function()
-            self.update_data()
-        print(self.movement)
-    
-
-    def move_random(self, x):
-        e = random() * (self.low_bound, self.up_bound) - 0.5
-        # equation - xi = xi + alpha*ei
-        new_pos = self.data[x] + (self.alpha * e)
-        return new_pos
+        # print(self.data)
+        for x in range(self.iter):
+            for i in range(self.population):
+                for j in range(self.population):
+                    if(self.fitness_value[j] < self.fitness_value[i]):
+                        if i == j: 
+                            continue
+                        self.data[i] = self.move_to(i, j)
+                        self.fitness_value[i] = self.fitness_function(self.data[i])
+            self.movement.append(self.data)
+            self.movement_fitness.append(self.fitness_value)
+        
 
     def move_to(self, x, y):
-        e = random() * (self.low_bound, self.up_bound) - 0.5
-        beta = self.beta * exp((self.gamma*-1) * (self.distance(self.data[x], self.data[y])**2))
+        data_x = self.data[x]
+        data_y = self.data[y]
 
-        new_pos = self.data[x] + beta + e
-        return new_pos
+        new_x = data_x + self.beta*(e**(self.gamma*-1 * (self.distance(data_x, data_y)**2)))*(data_x - data_y) + self.alpha * self.gamma * (self.generate_random() - 0.5) * self.scale
 
-    def distance(self, xi, xj):
+        if new_x < data_x:
+            return new_x
+        return data_x
+
+    def distance(self, xi:float, xj:float):
         return sqrt((xi - xj)**2)
-
+    
+    def generate_random(self):
+        return random()
+    
+    def print_result(self):
+        for i in self.data:
+            print(f'{i:.3f}')
 
     
